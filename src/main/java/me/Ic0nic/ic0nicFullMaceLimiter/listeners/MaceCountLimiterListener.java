79302239e70101252c4baeb61f2b72f1ab7407f8
Abdulla12e84 <221677888+Abdulla12e84@users.noreply.github.com>
2026-09-01 22:36:38 +0300

    Fix Crafter Bypassing Limit

--- src/main/java/me/Ic0nic/ic0nicFullMaceLimiter/listeners/MaceCountLimiterListener.java
+++ src/main/java/me/Ic0nic/ic0nicFullMaceLimiter/listeners/MaceCountLimiterListener.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.CrafterCraftEvent;
 import org.bukkit.event.entity.EntityRemoveEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
@@ -186,5 +187,11 @@ public class MaceCountLimiterListener implements Listener {
                 display.remove();
             }
         }
+    }
+
+    @EventHandler
+    public void onCrafterCrafting(CrafterCraftEvent event) {
+        if (event.getResult().getType() == Material.MACE)
+            event.setCancelled(true);
     }
 }
